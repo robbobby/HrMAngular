44a814f76a04909ee3275f02a2f0875a75169cc0
robbobby <69080884+robbobby@users.noreply.github.com>
2021-08-22 21:23:28 +0100

    Updated emplyoee spec

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  jobTitle: string;
+  department: string;
+  phoneNumber: string;
+  imageUrl: string;
+}
